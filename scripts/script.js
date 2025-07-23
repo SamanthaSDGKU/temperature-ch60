6acc11e3574afdafe1a8341dc20e107cc607dcad
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2025-07-22 18:06:59 -0700

    variables

---
+++ scripts/script.js
@@ -0,0 +1,65 @@
+console.log("Script");
+
+// VARIABLES
+
+//Strings (text) address, phonenumber, email, zip
+let firstName = "Samantha"; // assing the value
+let lastName = 'Jimenez';
+
+//numerical (int, float) math operation
+let age = 99;
+let mySize = 10;
+
+// boolean values (t/f)
+let isStudent = false;
+
+console.log(firstName);
+console.log(age,mySize);
+
+//concatenation (join strings)
+console.log("My name is " + firstName + " " + lastName);
+
+//Operations
+
+let num1 = 10; 
+let num2 = 2;
+
+let add = num1 + num2; 
+let subs = num1 - num2; 
+let div = num1/num2;
+let mult = num1 * num2;
+// subs,div, mult
+
+
+console.log("Add is: " + add);
+console.log("Subs is: " + subs);
+console.log("Div is: " + div);
+console.log("Mult is: " + mult);
+
+let a;
+const pi = 3.1416;
+let r = 3;
+
+a = pi * (r*r);
+console.log("The area of a circile with a radius of " + r + " is the " + a.toFixed(2));
+
+
+//creating the variables
+let aVar; // declare the variable
+aVar= 20;//assign the value
+let otherVar = 30;// declare and assign value
+
+console.log(aVar,otherVar);
+
+
+//change the values
+aVar = 50;
+otherVar = 60;
+
+console.log(aVar,otherVar);
+
+const weekDays = 5;
+
+//weekDays = 8; // the constant can't change
+
+console.log(weekDays);
